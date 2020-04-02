63150f46ddae458cf290fd424bf3ec029543d76e
Bicanic-Alen <bicanic.alen@iisgalvanimi.edu.it>
2020-04-02 18:55:50 +0000

    1.1.3

--- maps/src/app/app.component.ts
+++ maps/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Point } from 'src/models/point.model';
+
 
 @Component({
   selector: 'app-root',
@@ -14,20 +16,56 @@ export class AppComponent {
   lng2:number = 9.17265789;
 
   color:string;
+  colorR:string;
   constructor(){}
   onClickRed() : boolean{
     this.color="red";
     return false
   }
   onClickVerde() : boolean{
-    this.color="blue";
-    return false
-  }
-  onClickBlu() : boolean{
     this.color="green";
     return false
   }
+  onClickBlu() : boolean{
+    this.color="blue";
+    return false
+  }
 
+  onClickRYellow() : boolean{
+    this.colorR="yellow";
+    return false
+  }
+  onClickRVerde() : boolean{
+    this.colorR="green";
+    return false
+  }
+  onClickRBlu() : boolean{
+    this.colorR="blue";
+    return false
+  }
+
+  icon =  {
+    url:'/assets/img/99 - cat_acrobat.ico',
+    scaledSize: {
+      width: 60,
+      height: 60
+    }
+  };
+
+   triangle: Array<Point> =
+  [
+    {lng:9.1, lat:45.5},
+    {lng:9.0, lat:45.6},
+    {lng:9.0, lat:45.4}
+  ]
+
+  rettangolo: Array<Point> =
+  [
+    {lng:9.0, lat:45.6},
+    {lng:9.0, lat:45.4},
+    {lng:8.1, lat:45.4},
+    {lng:8.1, lat:45.6}
+  ]
 
 
 }
